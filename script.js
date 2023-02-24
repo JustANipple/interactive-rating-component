dd1310b9dc67f3b5baed1aae86f8b04e9b34d83c
JustANipple <prandisamuel99@gmail.com>
2023-02-24 13:00:06 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const buttons = document.querySelectorAll(".rating-nums button");
 const btnArray = Array.from(buttons);
+const root = document.documentElement;
 
 for(const btn of btnArray) {
-    btn.addEventListener("click", changeColors);
+    btn.addEventListener("click", colorsOnClick);
+    btn.addEventListener("mouseover", colorsOnHover);
+    btn.addEventListener("mouseout", colorsOnClick);
 }
 
-function changeColors() {
-    this.style.backgroundColor = "grey";
-    this.style.color = "white";
+function colorsOnClick() {
+    this.style.backgroundColor = "hsl(217, 12%, 63%)";
+    this.style.color = "hsl(0, 0%, 100%)";
+}
+
+function colorsOnHover() {
+    this.style.backgroundColor = "hsl(25, 97%, 53%)";
+    this.style.color = "hsl(0, 0%, 100%)";
+}
+
+function colorsOnDefault() {
+    this.style.backgroundColor = "hsl(213, 19%, 18%)";
+    this.style.color = "hsl(217, 12%, 63%)";
 }
